fcf8cb825bee633369c0ed3ab314b1ae3c393c43
Luke DeWitt <ledewitt@gmail.com>
2012-05-21 15:03:30 -0600

    Fleshing out the starting files

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,5 +1,31 @@
-require_relative 'request'
+require_relative '../lib/request'
+# AWS - Amazon Web Service
+# These are the items that are needed for a AWS Request:
+# 
+# AWS Access Key Id
+# Signature - Caculated by using secret access key
+# Time Stamp
+# Date
 
-describe Request do
+describe PortableHole do
+  it "has an AWS Access Key ID" do
+    
+  end
+  
+  it "has a Signature" do
+    
+  end
+  
+  it "has a Time Stamp" do
+    
+  end
+  
+  it "has a Date" do
+    
+  end
+  
+  it "creates a Request" do
+    
+  end
   
 end
