03b7bea1ddc55fbcc5235505dcef3e6439c5d3d5
Luke DeWitt <ledewitt@gmail.com>
2012-05-22 13:07:55 -0600

    Cleaning up starting RSpec file

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/request'
+require 'portable_hole/request'
 # AWS - Amazon Web Service
 # These are the items that are needed for a AWS Request:
 # 
@@ -12,11 +12,11 @@ describe PortableHole::Request do
     
   end
   
-  it "creates Signature" do
+  it "creates a Signature" do
     
   end
   
-  it "creates Time Stamp" do
+  it "creates a Time Stamp" do
     
   end
   
