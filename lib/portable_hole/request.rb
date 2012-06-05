029cd588d957242eb693e960a655df898d25c23f
James Edward Gray II <james@graysoftinc.com>
2012-06-05 16:08:45 -0500

    Starting a Request object with automatic timestamping.

--- lib/portable_hole/request.rb
+++ lib/portable_hole/request.rb
@@ -1,22 +1,43 @@
 # Starting point.
 
-require 'openssl'
-require 'base64'
+# require 'openssl'
+# require 'base64'
 
 module PortableHole
-  class Request 
-
-    def initialize(access_key, secret_key)
-      @access_key = access_key
-      @secret_key = secret_key
+  class Request
+    TIMESTAMP_FORMAT = "%a, %d %b %Y %H:%M:%S %z"
+    
+    def initialize(url, verb, content, headers)
+      @url     = url
+      @verb    = verb
+      @content = content
+      @headers = headers
     end
-
-    def sign(date, request_type, resource)
-      raw_hmac = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), @secret_key, string_to_sign(date, request_type, resource))
-      sig = Base64.encode64(raw_hmac).chomp
-
-      "Authorization: AWS %s:%s" % [@access_key, sig]
+    
+    attr_reader :url, :verb, :content, :headers
+    
+    def add_date_header(clock = Time)
+      headers["Date"] = clock.now.strftime(TIMESTAMP_FORMAT)
     end
+    
+    # def sign(...)
+    #   # TODO: add_date_header(...)
+    #   #       add_a..._header(...)
+    # end
 
+    # def initialize(access_key, secret_key)
+    #   @access_key = access_key
+    #   @secret_key = secret_key
+    # end
+    # 
+    # def sign(date, request_type, resource)
+    #   raw_hmac = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), @secret_key, string_to_sign(date, request_type, resource))
+    #   sig = Base64.encode64(raw_hmac).chomp
+    # 
+    #   "Authorization: AWS %s:%s" % [@access_key, sig]
+    # end
+    private
+    
+    # ...
   end
 end
--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -7,25 +7,47 @@ require 'portable_hole/request'
 # Time Stamp
 # Date
 
+class TestClock
+  def initialize(time)
+    @time = time
+  end
+  
+  def now
+    self
+  end
+  
+  def strftime(*args)
+    @time.strftime(*args)
+  end
+end
+
 describe PortableHole::Request do
-  it "uses an AWS Access Key ID" do
-    
+  let(:url)     { "http://example.com" }
+  let(:verb)    { "GET" }
+  let(:content) { nil }
+  let(:headers) { {"Content-Size" => 0} }
+  let(:request) { PortableHole::Request.new(url, verb, content, headers) }
+
+  it "stores a URL, a verb, some content, and some header" do
+    request.url.should     eq(url)
+    request.verb.should    eq(verb)
+    request.content.should be_nil
+    request.headers.should eq(headers)
   end
   
-  it "creates Signature" do
-    
+  it "adds a Date header" do
+    clock = TestClock.new(Time.now)
+    request.add_date_header(clock)
+    request.headers["Date"].should eq(
+      clock.strftime(PortableHole::Request::TIMESTAMP_FORMAT)
+    )
   end
   
-  it "creates Time Stamp" do
-    
-  end
-  
-  it "knows the Date" do
-    
-  end
-  
-  it "signs a Request" do
-    
-  end
-  
+  # it "adds a A... header" do
+  #   # TODO: request.add_a..._header(AWS_KEY, AWS_SECRET)
+  # end
+  # 
+  # it "signs a Request" do
+  #   # TODO: request.sign(AWS_KEY, AWS_SECRET, clock)
+  # end
 end
